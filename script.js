e299f81d264461bc8374f1f02e52bcceffe16409
Stephaniekari <83511445+Stephaniekari@users.noreply.github.com>
2024-07-21 14:57:22 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function calculatePrice() {
     const price = parseFloat(document.getElementById('price').value);
     const exchangeRate = parseFloat(document.getElementById('exchangeRate').value);
-    const profitMargin = parseFloat(document.getElementById('profitMargin').value);
 
-    if (isNaN(price) || isNaN(exchangeRate) || isNaN(profitMargin)) {
+    if (isNaN(price) || isNaN(exchangeRate)) {
         alert("Por favor, ingrese valores válidos.");
         return;
     }
 
     const priceInQuetzales = price * exchangeRate;
-    const finalPrice = priceInQuetzales * (1 + (profitMargin / 100));
+
+    let profitMargin;
+    if (priceInQuetzales <= 10) {
+        profitMargin = 80;
+    } else if (priceInQuetzales <= 50) {
+        profitMargin = 60;
+    } else if (priceInQuetzales <= 100) {
+        profitMargin = 40;
+    } else if (priceInQuetzales <= 200) {
+        profitMargin = 35;
+    } else {
+        profitMargin = 30;
+    }
+
+    const finalPrice = priceInQuetzales * (1 + (profitMargin / 100)) + 9; // Agregando Q9 de gastos de empaque y envío
 
     document.getElementById('finalPrice').textContent = finalPrice.toFixed(2);
 }
